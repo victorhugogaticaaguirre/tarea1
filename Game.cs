272ed176f1d4f90944f0cca7a37f3ecaa5fe22b8
asylum1994 <gaticaaguirrev@gmail.com>
2022-09-05 23:14:39 -0400

    first commit

---
+++ Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+//using OpenTK.Graphics.ES11;
+
+
+namespace tarea1
+{
+     class Game
+    {
+        GameWindow window;
+
+       public Game(GameWindow window)
+        {
+            this.window = window;
+            start();
+        }
+
+
+        void start()
+        {
+            window.Load += loaded;
+            window.Resize += resize; 
+            window.RenderFrame += renderF;
+            window.Run(1.0/60.0);
+        }
+
+        void resize(object o, EventArgs e)
+        {
+            GL.Viewport(0,0,window.Width,window.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho((float)0.0, (float)50.0, (float)0.0, (float)50.0, (float)-1.0, (float)1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+        }
+
+        void renderF(object o, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Begin(BeginMode.Triangles);
+             GL.Vertex2(1.0,1.0);
+             GL.Vertex2(49.0, 1.0);
+             GL.Vertex2(25.0, 49.0);
+            GL.Color3(1.0f, 0, 1.0f);
+            GL.End();
+            
+            window.SwapBuffers();
+        }
+        void loaded( object o,EventArgs e)
+        {
+            
+            GL.ClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES30;
+using OpenTK.Input;
+
+namespace tarea1
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            GameWindow window = new GameWindow();
+            Game gm = new Game(window);
+
+        }
+    }
+}
